4a53aebaadbef994705c41e6f01748bbcf071a9e
Jordan Roberts <jrobertswebdev@gmail.com>
2024-07-02 22:17:42 -0500

    validate session on editor layout

--- app/editor/layout.tsx
+++ app/editor/layout.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { validateRequest } from '@/actions/auth-actions'
+import { redirect } from 'next/navigation'
 import EditorNav from './editor-nav'
 
-function EditorLayout({ children }: { children: React.ReactNode }) {
+async function EditorLayout({ children }: { children: React.ReactNode }) {
+ const { user, session } = await validateRequest()
+ if (!user) {
+  return redirect('/login')
+ }
  return (
   <div>
    <div className='w-full h-full flex gap-2 p-2 pt-0'>
---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Profile() {
+ return (
+  <div>
+   <Link href='/'>Back</Link>
+   Profile
+  </div>
+ )
+}
+
+export default Profile
