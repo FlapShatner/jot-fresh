69d26a2870c1bc3e10db966ff2df49552771cf06
Jordan Roberts <jrobertswebdev@gmail.com>
2024-12-02 21:13:04 -0600

    add sending email indicator to reset email button

--- app/(auth)/reset/page.tsx
+++ app/(auth)/reset/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from 'react'
-import { Button } from '@/components'
+
+import { cn } from '@/lib/cn'
 import { resetPassword } from '@/actions/auth-actions'
 import { sendEmail } from '@/lib/nodemailer'
 import Link from 'next/link'
@@ -10,24 +11,28 @@ import ResetPasswordEmail from '@/components/reset-email'
 
 export default function Reset() {
  const [success, setSuccess] = useState(false)
+ const [isLoading, setIsLoading] = useState(false)
  const [email, setEmail] = useState('')
 
  async function handleSubmit(formData: FormData) {
+  setIsLoading(true)
   const email = formData.get('email')?.toString()
   if (!email) {
    return alert('Please enter an email')
   }
   const tok = await resetPassword(email)
   if (!tok || (typeof tok === 'object' && 'error' in tok)) {
+   setIsLoading(false)
    return alert('There was an error. Please try again')
   }
-  const resetLink = `https://jot-fresh.vercel.app/reset${tok}`
+  const resetLink = `https://jot-fresh.vercel.app/reset/${tok}`
   const emailHtml = await render(<ResetPasswordEmail resetPasswordLink={resetLink} />)
   const response = await sendEmail(email, 'Jot Password reset', emailHtml)
   if (response) {
    setEmail(response.accepted[0] as string)
    setSuccess(true)
   }
+  setIsLoading(false)
  }
 
  return (
@@ -60,7 +65,14 @@ export default function Reset() {
         className='w-full text-fg-primary bg-bg-primary border border-var-cyan-trans py-1 px-2 text-lg rounded-md hover:bg-bg-secondary '
        />
       </div>
-      <Button variant='primary'>Send reset link</Button>
+      <button
+       disabled={isLoading}
+       className={cn(
+        'w-full bg-accent rounded-md py-2 mt-4 text-bg-primary font-bold border border-accent hover:bg-accent-light hover:border-accent-light transition-all'
+       )}>
+       {isLoading ? 'Sending email...' : 'Send reset link'}
+      </button>
+
       <Link
        className='w-full bg-bg-primary rounded-md py-2 mt-4 text-accent-light font-bold border border-accent hover:bg-var-editor-active hover:border-accent-light transition-all text-center'
        href='/login'>
