e2e124f45aeef23da93d0064c96d68c70d5e5cf4
jordan <jrobertswebdev@gmail.com>
2025-09-16 22:37:02 -0700

    run codemod

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,21 @@
-export default {
- ignores: ['next.config.mjs'],
-}
+import nextPlugin from "@next/eslint-plugin-next";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+  {
+    ignores: ["next.config.mjs", ".next/", "node_modules/"],
+  },
+];
